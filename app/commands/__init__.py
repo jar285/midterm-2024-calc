be5ad2efc86b6a958e5bb34736cba66dbec2058d
jar285 <jar285@njit.edu>
2024-03-28 19:41:59 -0400

    Module docstring

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -1,3 +1,4 @@
+'''Command pattern is a behavioral design pattern that turns a request into a stand-alone object that contains all information about the request. This transformation lets you pass requests as a method arguments, delay or queue a request’s execution, and support undoable operations.'''
 from abc import ABC, abstractmethod
 
 class Command(ABC):
