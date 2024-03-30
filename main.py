8188ec6173d019c41a5e5d2189a16d4bbfe503aa
jar285 <jar285@njit.edu>
2024-03-29 20:56:45 -0400

    added pandas as a requirement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # main.py
-from app import App    
+from app import App
 
 # You must put this in your main.py because this forces the program to start when you run it from the command line.
 if __name__ == "__main__":
